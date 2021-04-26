40c47f9621f85fd6a506b60a233d8bc51bd856a8
Lady_Proger <facktartar@mail.com>
2021-04-26 11:31:27 +0300

    add handle change

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, {FormEvent} from 'react'
 import FileUploader from "../DragAndDrop/FileUploader";
+import './form.sass'
 
 export const Form = () => {
+    const handleSubmit = (event:FormEvent<HTMLFormElement>):void => {
+        event.preventDefault()
+    }
     return(
         <div className="card">
-            <h1>This is my form</h1>
-            <form className="form">
-                <div>
-                    <label htmlFor="form__email">Email:</label>
-                    <input type="email" className="form" id="email-form"/>
-                </div>
-                <div>
-                    <label htmlFor="form__comment">Comment:</label>
-                    <input type="text" className="comment" id="comment-form"/>
-                </div>
-                <FileUploader/>
-                <button type="submit" className="form__button">SEND</button>
-            </form>
+            <div>
+                <h1>This is my form</h1>
+                <form className="form" onSubmit={handleSubmit}>
+                    <label>
+                        <span>Email:</span>
+                        <input
+                            type="email"
+                            className={`form`}
+                            name={"email"}
+                            id="email-form"
+                            placeholder="Email"
+                        />
+                    </label>
+                    <label>
+                        <span>Comment:</span>
+                        <input
+                            type="text"
+                            className={`comment`}
+                            id="comment-form"
+                            placeholder="Comment"
+                        />
+                    </label>
+                    <FileUploader/>
+                    <button type="submit" className="form__button">SEND</button>
+                </form>
+            </div>
         </div>
     )
 }
